bf82dd2c5bab98c34239408e113d27c5b471a6e5
Arecurius <marcel@kirsti.de>
2019-06-29 22:09:50 +0200

    Update

--- src/Hud/Health/Healthbar.cs
+++ src/Hud/Health/Healthbar.cs
@@ -130,9 +130,5 @@ namespace PoeHUD.Hud.Health
         {
             return Life.CurHP + Life.CurES;
         }
-        public bool IsLegionAndHidden(EntityWrapper entity)
-        {
-            return entity.Path.Contains("LegionLeague") && !entity.IsActive;
-        }
     }
 }
--- src/Hud/Trackers/PoiTracker.cs
+++ src/Hud/Trackers/PoiTracker.cs
@@ -92,17 +92,14 @@ namespace PoeHUD.Hud.Trackers
             {
                 return new CreatureMapIcon(e, "ms-cyan.png", () => Settings.Masters, Settings.MastersIcon);
             }
-            if (e.HasComponent<NPC>() && cadiro.Contains(ePath))
-            {
-                return new CreatureMapIcon(e, "ms-green.png", () => Settings.Cadiro, Settings.CadiroIcon);
-            }
             if (e.HasComponent<Chest>() && perandus.Contains(ePath))
             {
                 return new ChestMapIcon(e, new HudTexture("strongbox.png", Settings.PerandusChestColor), () => Settings.PerandusChest, Settings.PerandusChestIconSize);
             }
+
             //Legion League Monster
             if (ePath.StartsWith("Metadata/Monsters/LegionLeague/"))
-            {   
+            {
                 //Generals
                 if (ePath.EndsWith("General"))
                 {
@@ -111,11 +108,6 @@ namespace PoeHUD.Hud.Trackers
                 //Legion Monster chests
                 if (ePath.StartsWith("Metadata/Monsters/LegionLeague/MonsterChest") || (e.HasComponent<Chest>() && !e.GetComponent<Chest>().IsOpened) && ePath.StartsWith("Metadata/Chests/LegionChests"))
                 {
-                    if (ePath.Contains("BreachChest"))
-                    {
-                        return new ChestMapIcon(e, new HudTexture("strongbox.png", Settings.BreachChestColor), () => Settings.BreachChest, Settings.BreachChestIcon);
-                    }
-
                     if (ePath.StartsWith("Metadata/Chests/LegionChests"))
                     {
                         // Parse Legion Chests
@@ -123,6 +115,11 @@ namespace PoeHUD.Hud.Trackers
                         {
                             // epic Legion chests with no crystal
                             return new ChestMapIcon(e, new HudTexture("strongbox.png", Settings.LegionEpicNoCrystalChestColor), () => Settings.LegionChest, Settings.LegionEpicNoCrystalChestIcon);
+                        }
+                        if (ePath.Contains("2"))
+                        {
+                            // Epic Legion chests (with a crystal?)
+                            return new ChestMapIcon(e, new HudTexture("strongbox.png", Settings.LegionEpicChestColor), () => Settings.LegionChest, Settings.LegionEpicChestIcon);
                         }
                         if (ePath.Contains("Epic"))
                         {
@@ -137,51 +134,13 @@ namespace PoeHUD.Hud.Trackers
                         // Legion chests (with a crystal?)
                         return new ChestMapIcon(e, new HudTexture("strongbox.png", Settings.LegionChestColor), () => Settings.LegionChest, Settings.LegionChestIcon);
                     }
-                    if (ePath.Contains("2"))
-                    {
-                        return new ChestMapIcon(e, new HudTexture("strongboxes/chest_divination.png",
-                                                                  e.GetComponent<ObjectMagicProperties>().Rarity), () => Settings.Strongboxes, Settings.StrongboxesIconSize);
-                    }
-                    if (chest.IsStrongbox)
-                    {
-                        var chestIcon = "chest.png";
-                        switch (ePath)
-                        {
-                            case "Metadata/Chests/StrongBoxes/StrongboxDivination":
-                                chestIcon = "chest_divination.png"; break;
-                            case "Metadata/Chests/StrongBoxes/Ornate":
-                                chestIcon = "chest_ornate.png"; break;
-                            case "Metadata/Chests/StrongBoxes/Large":
-                                chestIcon = "chest_large.png"; break;
-                            case "Metadata/Chests/StrongBoxes/Jeweller":
-                                chestIcon = "chest_jewelers.png"; break;
-                            case "Metadata/Chests/StrongBoxes/Gemcutter":
-                                chestIcon = "chest_gemscutter.png"; break;
-                            case "Metadata/Chests/StrongBoxes/Artisan":
-                                chestIcon = "chest_quality.png"; break;
-                            case "Metadata/Chests/StrongBoxes/Armory":
-                                chestIcon = "chest_weapon.png"; break;
-                            default:
-                                if (ePath.StartsWith("Metadata/Chests/StrongBoxes/Cartographer"))
-                                    chestIcon = "chest_map.png";
-                                else if (ePath.StartsWith("Metadata/Chests/StrongBoxes/Arcanist"))
-                                    chestIcon = "chest_no_quality.png";
-                                break;
-                        }
-
-
-                        return new ChestMapIcon(e, new HudTexture("strongboxes/" + chestIcon,
-                                e.GetComponent<ObjectMagicProperties>().Rarity), () => Settings.Strongboxes, Settings.StrongboxesIconSize);
-                    }
-                    // Legion chests (with a crystal?)
-                    return new ChestMapIcon(e, new HudTexture("strongbox.png", Settings.LegionChestColor), () => Settings.LegionChest, Settings.LegionChestIcon);
                 }
             }
-            
+
             //chests
             if (e.HasComponent<Chest>() && !e.GetComponent<Chest>().IsOpened)
             {
-	            if (ePath.Contains("BreachChest"))
+                if (ePath.Contains("BreachChest"))
                 {
                     return new ChestMapIcon(e, new HudTexture("strongbox.png", Settings.BreachChestColor), () => Settings.BreachChest, Settings.BreachChestIcon);
                 }
@@ -194,6 +153,39 @@ namespace PoeHUD.Hud.Trackers
                     return new ChestMapIcon(e, new HudTexture("strongboxes/chest_divination.png",
                                                               e.GetComponent<ObjectMagicProperties>().Rarity), () => Settings.Strongboxes, Settings.StrongboxesIconSize);
                 }
+                if (e.GetComponent<Chest>().IsStrongbox)
+                {
+                    var chestIcon = "chest.png";
+                    switch (ePath)
+                    {
+                        case "Metadata/Chests/StrongBoxes/StrongboxDivination":
+                            chestIcon = "chest_divination.png"; break;
+                        case "Metadata/Chests/StrongBoxes/Ornate":
+                            chestIcon = "chest_ornate.png"; break;
+                        case "Metadata/Chests/StrongBoxes/Large":
+                            chestIcon = "chest_large.png"; break;
+                        case "Metadata/Chests/StrongBoxes/Jeweller":
+                            chestIcon = "chest_jewelers.png"; break;
+                        case "Metadata/Chests/StrongBoxes/Gemcutter":
+                            chestIcon = "chest_gemscutter.png"; break;
+                        case "Metadata/Chests/StrongBoxes/Artisan":
+                            chestIcon = "chest_quality.png"; break;
+                        case "Metadata/Chests/StrongBoxes/Armory":
+                            chestIcon = "chest_weapon.png"; break;
+                        default:
+                            if (ePath.StartsWith("Metadata/Chests/StrongBoxes/Cartographer"))
+                                chestIcon = "chest_map.png";
+                            else if (ePath.StartsWith("Metadata/Chests/StrongBoxes/Arcanist"))
+                                chestIcon = "chest_no_quality.png";
+                            break;
+                    }
+
+
+                    return new ChestMapIcon(e, new HudTexture("strongboxes/" + chestIcon,
+                            e.GetComponent<ObjectMagicProperties>().Rarity), () => Settings.Strongboxes, Settings.StrongboxesIconSize);
+                }
+
+                return new ChestMapIcon(e, new HudTexture("chest.png"), () => Settings.Chests, Settings.ChestsIcon);
             }
 
             if (ePath == "Metadata/Terrain/Leagues/Synthesis/Objects/SynthesisAirlockController")
